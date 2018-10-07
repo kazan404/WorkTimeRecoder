d893f408af98795b58912c820421ed98faa87d6a
sagari <kazankidg@hotmail.co.jp>
2018-10-07 20:54:21 +0900

    #1 時間計算の基本メソッドを追加

--- WorkTimeRecoder/TimeCulcurater.cs
+++ WorkTimeRecoder/TimeCulcurater.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace WorkTimeRecoder
 {
-    class TimeCulcurater
+    class TimeCounter : ITimeCounter
     {
+        DateTime startTime = DateTime.Now;
+        DateTime stopTime = DateTime.Now;
+
+        public void StartCount()
+        {
+            startTime = DateTime.Now;
+        }
+        public void StopCount()
+        {
+            stopTime = DateTime.Now;
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public TimeSpan GetCountTime()
+        {
+            TimeSpan currentTime = DateTime.Now - startTime;
+            return currentTime;
+        }
+        public string GetCountTime(string format)
+        {
+            TimeSpan currentTime = GetCountTime();
+            return currentTime.ToString(format);
+        }
     }
 }
