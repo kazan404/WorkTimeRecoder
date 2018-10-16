2c164a1ecf117b8437a9e2ca24f9afb385fe0e99
sagari <kazankidg@hotmail.co.jp>
2018-10-16 13:52:04 +0900

    タスクトレイへアイコンと仮のメニューを追加

---
+++ WorkTimeRecoder/NotifyIconWrapper.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WorkTimeRecoder
+{
+    partial class NotifyIconWrapper
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NotifyIconWrapper));
+            this.timerNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timerContextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem_Open = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem_Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerContextMenuStrip.SuspendLayout();
+            // 
+            // timerNotifyIcon
+            // 
+            this.timerNotifyIcon.ContextMenuStrip = this.timerContextMenuStrip;
+            this.timerNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("timerNotifyIcon.Icon")));
+            this.timerNotifyIcon.Text = "This is a work time counter.";
+            this.timerNotifyIcon.Visible = true;
+            // 
+            // timerContextMenuStrip
+            // 
+            this.timerContextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem_Open,
+            this.toolStripMenuItem_Exit});
+            this.timerContextMenuStrip.Name = "timerContextMenuStrip";
+            this.timerContextMenuStrip.Size = new System.Drawing.Size(99, 48);
+            // 
+            // toolStripMenuItem_Open
+            // 
+            this.toolStripMenuItem_Open.Name = "toolStripMenuItem_Open";
+            this.toolStripMenuItem_Open.Size = new System.Drawing.Size(98, 22);
+            this.toolStripMenuItem_Open.Text = "表示";
+            // 
+            // toolStripMenuItem_Exit
+            // 
+            this.toolStripMenuItem_Exit.Name = "toolStripMenuItem_Exit";
+            this.toolStripMenuItem_Exit.Size = new System.Drawing.Size(98, 22);
+            this.toolStripMenuItem_Exit.Text = "終了";
+            this.timerContextMenuStrip.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon timerNotifyIcon;
+        private System.Windows.Forms.ContextMenuStrip timerContextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_Open;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem_Exit;
+    }
+}
---
+++ WorkTimeRecoder/NotifyIconWrapper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WorkTimeRecoder
+{
+    public partial class NotifyIconWrapper : Component
+    {
+        /// <summary>
+        /// NotifyIconWrapper クラス を生成、初期化します。
+        /// </summary>
+        public NotifyIconWrapper()
+        {
+            InitializeComponent();
+
+            this.toolStripMenuItem_Open.Click += ToolStripMenuItem_Open_Click;
+            this.toolStripMenuItem_Exit.Click += ToolStripMenuItem_Exit_Click;
+        }
+
+        /// <summary>
+        /// コンテナ を指定して NotifyIconWrapper クラス を生成、初期化します。
+        /// </summary>
+        /// <param name="container">コンテナ</param>
+        public NotifyIconWrapper(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// コンテキストメニュー "表示" を選択したとき呼ばれます。
+        /// </summary>
+        /// <param name="sender">呼び出し元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void ToolStripMenuItem_Open_Click(object sender, EventArgs e)
+        {
+            // MainWindow を生成、表示
+            MainWindow window = new MainWindow();
+            window.Show();
+        }
+
+        /// <summary>
+        /// コンテキストメニュー "終了" を選択したとき呼ばれます。
+        /// </summary>
+        /// <param name="sender">呼び出し元オブジェクト</param>
+        /// <param name="e">イベントデータ</param>
+        private void ToolStripMenuItem_Exit_Click(object sender, EventArgs e)
+        {
+            // 現在のアプリケーションを終了
+            Application.Current.Shutdown();
+        }
+    }
+}
