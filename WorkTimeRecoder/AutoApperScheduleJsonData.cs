938fc516a4b917e13eb1dbcb0ecfd9e585a42787
kogawa <kogawa.kentaro.pfu@jp.fujitsu.com>
2020-06-18 19:02:42 +0900

    JSONのシリアライズデータとメソッドを作成

---
+++ WorkTimeRecoder/AutoApperScheduleJsonData.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace WorkTimeRecoder
+{
+    [JsonObject("AutoApperSchedule")]
+    class AutoApperScheduleJsonData
+    {
+        [JsonProperty("Schedule Name")]
+        public string Name { get; set; }
+
+        [JsonProperty("Schedule IDNumber")]
+        public string IDNumber { get; set; }
+
+        private AutoApperScheduleJsonData()
+        {
+
+        }
+
+        public AutoApperScheduleJsonData(string scheduleName, string scheduleIDNumber)
+        {
+            Name = scheduleName;
+            IDNumber = scheduleIDNumber;
+        }
+    }
+}
---
+++ WorkTimeRecoder/JsonTransrater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WorkTimeRecoder
+{
+    static class JsonTransrater
+    {
+        public static string SerializeToJson(object serializeObject)
+        {
+            return JsonConvert.SerializeObject(serializeObject);
+        }
+
+        public static T DesirializeFromJson<T>(string desirializeJsonData)
+        {
+            return JsonConvert.DeserializeObject<T>(desirializeJsonData);
+        }
+    }
+}
